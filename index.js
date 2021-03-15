a28e7f78abf41c83d30265ccb26760d4d2129d82
Rohan0310 <rpalkar@confederationcollege.ca>
2021-03-14 21:56:49 -0400

    Completed till step 11

--- index.js
+++ index.js
@@ -2,12 +2,40 @@ const express = require('express')
 
 const morgan = require('morgan')
 
+const bodyParser = require('body-parser');
+
 const app = express()
-app.use(morgan('combined'))
+app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (request, response) => {
    // console.log('Request,Received')
     //response.json({msg: 'Welcome'});
+   // response.end()
+
+//    //Query
+//    console.log('Query parameter')
+//    console.log(request.query)
+
+   //Response
+//    console.log('Query parameter')
+//    console.log(request.params)
+
+   //response.send(request.query['first_name'])
+ 
+   //Body
+   console.log('Body parameter')
+    console.log(request.body)
+    
+
+   response.end()
+})
+app.get('/:first_name/:last name/:age', (request, response) => {
+
+    //Route
+    console.log('Route parameter')
+    console.log(request.params)
     response.end()
 })
 app.listen(3333, () => {
